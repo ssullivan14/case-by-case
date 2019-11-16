54e8623b145ada75db7715b31dc30f1e934fc19d
sara sullivan <saralsullivan@gmail.com>
2019-11-16 13:38:05 -0500

    alt user info in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,6 @@ USE casebycase;
 
 INSERT INTO user (user_name, user_email, user_password)
 VALUES ("ssullivan", "saralsullivan@gmail.com", "cr!m3junk!3"),("dsullivan46", "sully804@gmail.com", "Fr33Qu1ncy!");
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
