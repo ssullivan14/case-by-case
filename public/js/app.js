170f8906e253f5f42d7a755795648aaa86bc4273
Bishop Bader <bflatbader@gmail.com>
2019-11-19 21:30:25 -0500

    Addded home page for after login

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+    // GET request to determine which user is logged in
+    $.get("/api/user_data").then(function(data) {
+        console.log(data);
+        
+        if (data.username) {
+            $(".member-name").text(data.username);
+        } else {
+            // For testing
+            $(".member-name").text("Default");
+        }
+      });
 
-	
+    // Load date picker to search form
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    var options={
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      };
+      date_input.datepicker(options);
 });
