51cf95232fd3a60a6daf3035d700d4e450eef50d
Bishop Bader <bflatbader@users.noreply.github.com>
2019-11-23 00:03:55 -0500

    fixed namusdata.js, got database to query and console.log

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -46,6 +46,22 @@ module.exports = function(app) {
   });
 
 
+  app.get("/search", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    console.log(req.query);
+    
+    db.Person_missing.findAll({
+      where: {
+        City_Of_Last_Contact: req.query.City_Of_Last_Contact,
+        State_Of_Last_Contact: req.query.State_Of_Last_Contact
+      }
+    }).then(function(dbPerson_missing) {
+        console.log(dbPerson_missing);
+    });
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
